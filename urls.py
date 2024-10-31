0b373fe6046c0d5ba40c16b882a300c85aeebb03
Gabriel Albuquerque da Silva <82517993+Gadsz@users.noreply.github.com>
2024-10-31 18:54:15 -0300

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import ArtistaListCreateView, AlbumListCreateView
+
+# Ao abrir o Insomnia, ejetar o Ctrl J para abrir terminal (No VScode) e executar "python3 manage.py runserver" 
+
+urlpatterns = [
+    path('artistas/', ArtistaListCreateView.as_view(), name='artista-list-create'), # E esse é o de artista, o que muda
+    #"path('NOME DA VARIÁVEL/', NOME DA VARIÁVELlistCreateView.as_view(), name='NOME DA VARIÁVEL-list-create'),"
+    path('album/', AlbumListCreateView.as_view(), name='album-list-create'), # Esse é o caminho para criar (CRUD) de álbum
+]
+
+# Nesta parte quando abrir o Insomnia o que será adicionado na url da new request será o nome do path que é o
+# nome da view que irei trabalhar.
